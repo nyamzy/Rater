ceea64ebe3df5bbc290c565224ecbc325842df0f
nyamzy <john.nyamweya@student.moringaschool.com>
2022-02-13 19:54:30 +0300

    create the save method

--- rated/models.py
+++ rated/models.py
@@ -6,10 +6,18 @@ class Profile(models.Model):
     bio = models.CharField(max_length=200, null = True)
     phone_number = models.CharField(max_length=20, null = True)
 
+    def __str__(self):
+        return self.bio
+    
+    # Save method
+    def save_profile(self):
+        self.save()
+
+    # 
+
 
 class Projects(models.Model):
     title = models.CharField(max_length=30)
     landing_page = models.ImageField(upload_to = 'projects/')
     description = models.CharField(max_length=100)
     link = models.URLField(max_length=200)
-    
