50bc316ec588ab26fc5c209e7c23fdb4cd6e9e12
nyamzy <john.nyamweya@student.moringaschool.com>
2022-02-13 18:41:14 +0300

    create model classes for projects and profile

--- rated/models.py
+++ rated/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    prof_pic = models.ImageField(upload_to = 'projetcs/')
+    bio = models.CharField(max_length=200, null = True)
+    phone_number = models.CharField(max_length=20, null = True)
+
+
+class Projects(models.Model):
+    title = models.CharField(max_length=30)
+    landing_page = models.ImageField(upload_to = 'projects/')
+    description = models.CharField(max_length=100)
+    link = models.URLField(max_length=200)
+    
