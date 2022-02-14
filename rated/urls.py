8003d78843479467b3a9c15dcb4ce7dc371c6336
nyamzy <john.nyamweya@student.moringaschool.com>
2022-02-14 12:37:44 +0300

    create url for search results

--- rated/urls.py
+++ rated/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
 from django.conf import settings
-from django.conf.urls import static
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('search/', views.search, name='search_results'),
 ]
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
