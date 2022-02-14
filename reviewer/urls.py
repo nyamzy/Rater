c4b9dcfbcdd556a12997679f4351abf5f989d9d7
nyamzy <john.nyamweya@student.moringaschool.com>
2022-02-14 13:25:59 +0300

    install tinymce editor

--- reviewer/urls.py
+++ reviewer/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('', include('rated.urls')),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('tinymce/', include('tinymce.urls')),
 ]
