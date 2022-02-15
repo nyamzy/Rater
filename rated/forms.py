580a387979e382b3706330cbbac36f2b63838388
nyamzy <john.nyamweya@student.moringaschool.com>
2022-02-15 15:53:46 +0300

    create a create profile form

--- rated/forms.py
+++ rated/forms.py
@@ -1,8 +1,12 @@
-from .models import Projects
+from .models import Profile, Projects
 from django import forms
 
 class PostProjectForm(forms.ModelForm):
     class Meta:
         model = Projects
         exclude = ['user']
-        
+
+class CreateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user']
